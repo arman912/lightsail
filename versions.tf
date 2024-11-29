f814df538eef1d33e009601d9c26f854b5fcdb19
Amandeep.Singh.25365 <amandeep.singh@microfocus.com>
2024-11-29 12:14:07 +0000

    added setup files and created VPC in London

--- versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 1.5.0" # Adjust to the latest stable version
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" # Latest major version for AWS provider
-    }
-  }
-}
